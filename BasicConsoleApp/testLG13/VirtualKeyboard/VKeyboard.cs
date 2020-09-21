ce246b0ef81ed3ce5be6eec3b6a4da25f6060f7d
flyingoverclouds <nicolas.clerc@sablefin.net>
2020-09-22 00:43:48 +0200

    pinvoke declaration for User32

---
+++ BasicConsoleApp/testLG13/VirtualKeyboard/VKeyboard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testLG13.VirtualKeyboard
+{
+    /// <summary>
+    /// Virtual keyboard : used to simulate keystroke from a programme
+    /// </summary>
+    class VKeyboard
+    {
+       
+
+
+
+
+    }
+}
