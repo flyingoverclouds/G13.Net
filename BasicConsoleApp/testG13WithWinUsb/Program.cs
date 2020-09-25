36c9e932db8bc6d5580dc7f4ebea39ccaa015243
flyingoverclouds <nicolas.clerc@sablefin.net>
2020-09-25 19:07:22 +0200

    adding winusb test skeleton

---
+++ BasicConsoleApp/testG13WithWinUsb/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using PInvoke;
+
+namespace testG13WithWinUsb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test WinUSB!");
+
+                        
+        }
+    }
+}
