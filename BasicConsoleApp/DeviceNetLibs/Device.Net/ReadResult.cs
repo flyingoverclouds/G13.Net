ed4381f08f2735b431d9297c3f8c9a79a9e1d6e6
flyingoverclouds <nicolas.clerc@sablefin.net>
2020-10-08 22:38:59 +0200

    adding devicenet libraries for test

---
+++ BasicConsoleApp/DeviceNetLibs/Device.Net/ReadResult.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Device.Net
+{
+    public struct ReadResult
+    {
+        #region Public Properties
+        public byte[] Data { get; }
+
+
+        //TODO: Int or Uint?
+        public uint BytesRead { get; }
+        #endregion
+
+        #region Conversion Operators
+        public static implicit operator byte[](ReadResult readResult)
+        {
+            return readResult.Data;
+        }
+
+        public static implicit operator ReadResult(byte[] data)
+        {
+            //TODO: This is a bit dodgy... It's breaking a code rule
+            return data == null ? throw new ArgumentNullException(nameof(data)) : new ReadResult(data, (uint)data.Length);
+        }
+        #endregion
+
+        #region Constructor
+        public ReadResult(byte[] data, uint bytesRead)
+        {
+            Data = data;
+            BytesRead = bytesRead;
+        }
+        #endregion
+    }
+}
